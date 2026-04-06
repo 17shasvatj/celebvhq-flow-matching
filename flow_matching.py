ad39dc779d95594f12dd56e5ba15d73067c6e955
17shasvatj <shas.atman@gmail.com>
2026-04-06 10:49:07 -0700

    Add gradient clipping

--- flow_matching.py
+++ flow_matching.py
@@ -14,6 +14,7 @@ def train_step(model, x1, optimizer):
     loss = F.mse_loss(v_pred, v)
     optimizer.zero_grad()
     loss.backward()
+    torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
     optimizer.step()
     return loss.item()
 
